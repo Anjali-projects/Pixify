83896c36531edbafa4381616d9568132b38abb68
Anjali <115994877+Anjali-projects@users.noreply.github.com>
2024-05-21 21:24:34 +0530

    changes done

--- src/Components/HrMovieCard.jsx
+++ src/Components/HrMovieCard.jsx
@@ -5,10 +5,10 @@ function HrMovieCard({movie}) {
   return (
     <section className='hover:scale-110 transition-all duration-150 ease-in'>
     <img src={IMAGE_BASE_URL+movie.backdrop_path} 
-    className='w-[110px] md:w-[260px] rounded-lg
+    className='w-[190px] md:w-[260px] rounded-lg
     hover:border-[3px] border-gray-400 cursor-pointer
     '/>
-    <h2 className='w-[110px] md:w-[260px] text-white
+    <h2 className='w-[190px] md:w-[260px] text-white
     mt-2'>{movie.title}</h2>
     </section>
   )
